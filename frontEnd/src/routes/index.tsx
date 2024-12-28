2ab7507708c86645ef59ed251b0e955370ec0739
MawmartA7 <aaronstmart@gmail.com>
2024-12-28 13:36:14 -0300

    refactor(frontend): simplify logged layout import to routes

--- frontEnd/src/routes/index.tsx
+++ frontEnd/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import { LogedLayout } from '../shared/layouts/LogedLayout'
+import { LogedLayout } from '../shared/layouts'
 
 export const Router = () => {
   return (
